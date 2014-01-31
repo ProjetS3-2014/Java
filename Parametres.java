12fe19c2f08635a0b25406be9e73602ff57a0f09
SBrav0 <sbravo@iut.univ-paris8.fr>
2014-01-31 04:32:17 +0100

    Create Parametres.java

---
+++ Parametres.java
@@ -0,0 +1,22 @@
+package com.example.projet;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Parametres extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_parametres);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.parametres, menu);
+		return true;
+	}
+
+}
